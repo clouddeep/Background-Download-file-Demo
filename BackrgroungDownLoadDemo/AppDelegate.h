dd6a9a85f0f37ce356e8337d5ffe38f95249d5ef
clouddeep <shoujhengduan@gmail.com>
2015-04-15 13:36:29 +0800

    Initial Commit

---
+++ BackrgroungDownLoadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackrgroungDownLoadDemo
+//
+//  Created by Shou Cheng Tuan  on 2015/4/15.
+//  Copyright (c) 2015年 Shou Cheng Tuan . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BackrgroungDownLoadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackrgroungDownLoadDemo
+//
+//  Created by Shou Cheng Tuan  on 2015/4/15.
+//  Copyright (c) 2015年 Shou Cheng Tuan . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
